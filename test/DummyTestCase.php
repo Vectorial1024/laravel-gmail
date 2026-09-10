1249b7066ff52800af1fa089abda7eed5e062da4
Vincent Wong <vincentwwy168@gmail.com>
2026-09-10 23:25:43 +0800

    Dummy test case

---
+++ test/DummyTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vectorial1024\LaravelGmail\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTestCase extends TestCase
+{
+    public function testPass()
+    {
+        $this->expectNotToPerformAssertions();
+    }
+}
